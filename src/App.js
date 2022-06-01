59ad55bb4e8e7dc03d9a378b89df00461e878e70
Jarosław Bąk <jarodelphi@gmail.com>
2022-06-01 13:03:13 +0200

    api coordinates

--- src/App.js
+++ src/App.js
@@ -9,14 +9,24 @@ function App() {
 
   const [places, setPlaces] = useState([]);
 
+  const [ coordinates, setCoordinates] = useState({});
+  const [ bounds, setBounds] = useState(null);
+
   useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({coords:{latitude, longitude}}) => {
+      setCoordinates({lat: latitude, lng: longitude})
+    })
+  })
+
+  useEffect(() => {
+    console.log(coordinates, bounds)
 
     getPlacesData()
       .then((data) => {
         console.log(data)
         setPlaces(data)
       })
-  },[])
+  },[coordinates,bounds])
   
   return (
     <>
@@ -27,7 +37,11 @@ function App() {
           <List />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map />
+          <Map 
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+          />
         </Grid>
       </Grid>
     </>
